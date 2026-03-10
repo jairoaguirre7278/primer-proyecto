461171eb39cbca169279c085a804a9f73662e257
Jairo Aguirre <Jaguirre7278@unisalamanca.edu.co>
2026-03-09 20:16:29 -0500

    clase 2

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class estudiante(BaseModel):
+    telefono: str
+    price: float
+    is_offer: bool | None = None
 
 
 @app.get("/")
@@ -11,3 +17,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, estudianteActualizacion: estudiante):
+    return {"estudiante_telefono": estudianteActualizacion.telefono, "item_id": item_id}
