349d328b9a0d00e7bb5379e8f6e363986594f5a7
Jairo Aguirre <Jaguirre7278@unisalamanca.edu.co>
2026-03-16 20:12:22 -0500

    clase 3

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Query
 from pydantic import BaseModel
+from typing import Annotated
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
 
 app = FastAPI()
+class estudianteModelo(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    nombre: str = Field(max_length=30)
 
+
+# Code above omitted 👆
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+# Code below omitted 👇
 class estudiante(BaseModel):
     telefono: str
     price: float
     is_offer: bool | None = None
 
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/estudiantes")
+def crear_estudiante(estudiante: estudianteModelo, session: SessionDep)-> estudianteModelo:
+    session.add(estudiante)
+    session.commit()
+    session.refresh(estudiante)
+    return estudiante
+
+@app.get("/estudiantes")
+def read_root(session: SessionDep):
+    estudiantes= session.exec(select(estudianteModelo)).all()
+    return estudiantes
 
 
 @app.get("/items/{item_id}")
